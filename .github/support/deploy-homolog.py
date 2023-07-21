df6f2e6e02ece9a307f570bd50573f25e5ba5b3a
arthur_melo <arthurmelo1000@gmail.com>
2023-07-21 16:29:51 -0300

    fix: exclude whitespaces on deploy-homolog python file

--- .github/support/deploy-homolog.py
+++ .github/support/deploy-homolog.py
@@ -15,12 +15,44 @@ def parseBranches(payload):
     # strip whitespaces from each branch
     return [branch.strip() for branch in branches]
 
+def getBranches(currentList, mergeList):
+    matches = re.search("##\s+branches\s*([^#]*)", mergeList, re.IGNORECASE + re.MULTILINE)
+
+    # Strip matches by end line character and get an array of branches
+    branches = [branch.strip() for branch in matches.group(1).splitlines()]
+
+    # Remove empty branches
+    branches = list(filter(None, branches))
+
+    # percorra branches e verifique as que contém _DELETE_ no nome. Adicione em deleteBranches e remova-as de branches
+    deleteBranches = []
+    for branch in branches:
+        if '_DELETE_' in branch:
+            deleteBranches.append(branch.replace('_DELETE_', '').strip())
+            branches.remove(branch)
+
+    # Remove deleteBranches from branches
+    branches =
+    currentList = [branch.strip() for branch in currentList.splitlines()]
+    branches = [branch for branch in currentList if branch not in deleteBranches]
+    print(branches)
+    return
+
+    # strip whitespaces from each branch
+    return [branch.strip() for branch in branches]
+
 try:
     command = sys.argv[1]
-    payload = sys.argv[2]
+    currentList = sys.argv[2]
+    mergeList = sys.argv[3]
 
     if command == 'parse-branches':
-        print('\n'.join(parseBranches(payload)))
+        print('\n'.join(parseBranches(mergeList)))
+        exit()
+
+    if command == 'get-branches':
+        getBranches(currentList, mergeList)
+        # print('\n'.join(getBranches(currentList, mergeList)))
         exit()
 
     print('["invalid-command"]')
